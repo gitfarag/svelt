0e32a5d7c093dd542f85e415b2fce92509e6757c
ahmed <remove>
2022-10-15 11:50:30 +0200

    sat 15/10

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,17 @@
+<script>
+  let count = 0
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<button on:click={increment}>
+  increment
+  
+</button>
+<button on:click={()=>count > 0 ? count-=1 : ''}>
+  decrement
+  
+</button>
+<button on:click={()=>count=0}>reset</button>
+count is {count}
